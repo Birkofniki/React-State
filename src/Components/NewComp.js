cce4628d2a04d5ff05d3055e6c71ba02978174d7
BirkofNiki <augastinendeti@gmail.com>
2024-04-29 00:13:35 +0300

    added react state code to change both the bell and message displayed when the user subscribes, and clicks on the bell icon

--- src/Components/NewComp.js
+++ src/Components/NewComp.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import bellA from "./bellA.png";
+import bellC from "./bellC.png";
+
 
 export class NewComp extends Component {
 
@@ -7,7 +10,8 @@ export class NewComp extends Component {
     
       this.state = {
          message: "Subscribe to my channel and follow me on SimpliLearn",
-         sub:"Subscribe here"
+         sub:"Subscribe here",
+         imageURL: bellA
       }
     }
 
@@ -20,14 +24,29 @@ export class NewComp extends Component {
         this.setState({
             message: "Hit the bell icon to never miss an update",
             sub: "Subscribed"
-        })
-      }
+        });
+      };
+      
+imageChange=()=> {
+  this.setState({
+    imageURL: bellC,
+    message: "Thank you, happy learning"
+  })
+}
+
 
   render() {
     return (
       <div className='App'>
         <h3 style={this.styles}> {this.state.message} </h3>
         <button onClick={this.ButtonChange}> {this.state.sub} </button>
+        <p></p>
+        <img 
+          style={ {width:"30px" , height: "30px"} } 
+          src={this.state.imageURL} 
+          onClick={this.imageChange}
+          alt="" 
+        />
 
       </div>
     )
